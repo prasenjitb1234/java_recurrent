3018acbeea2690bb45764abaa3fa0b7df7d65c47
Prasenjit <prasenjitbhosale1234@gmail.com>
2024-10-29 22:21:47 +0530

    Updated App.java, User.java

--- HibernateCrudOperation/src/main/java/com/learn/App.java
+++ HibernateCrudOperation/src/main/java/com/learn/App.java
@@ -12,14 +12,29 @@ public class App
     public static void main( String[] args )
     {
     	
-    	User user1 = new User();
-    	user1.setId(1);
-    	user1.setName("prasenjit");
-    	user1.setEmail("prasenjit@gmail.com");
-    	user1.setPassword("12345");
-    	user1.setGender("male");
-    	user1.setCity("Mumbai");
+//    	User user1 = new User();
+//    	user1.setId(1);
+//    	user1.setName("prasenjit");
+//    	user1.setEmail("prasenjit@gmail.com");
+//    	user1.setPassword("12345");
+//    	user1.setGender("male");
+//    	user1.setCity("Mumbai");
     	
+//    	 User user2 = new User();
+//    	 user2.setName("Wanda Maximoff"); 
+//    	 user2.setEmail("wanda.maximoff@example.com");
+//    	 user2.setPassword("hexmagic456");
+//    	 user2.setGender("female");
+//    	 user2.setCity("Westview");
+
+    	User user3 = new User();
+    	user3.setName("Peter Parker"); 
+    	user3.setEmail("peter.parker@gmail.com");
+    	user3.setPassword("webslinger2023");
+    	user3.setGender("male");
+    	user3.setCity("New York");
+
+
         
     	Configuration cfg = new Configuration();
     	cfg.configure("/com/learn/config/hibernate.cfg.xml");
@@ -30,7 +45,7 @@ public class App
     	Transaction transaction = session.beginTransaction();
     	
     	try {
-    		session.save(user1);
+    		session.save(user3);
         	transaction.commit();
     	}
     	catch(Exception e) {
--- HibernateCrudOperation/src/main/java/com/learn/entities/User.java
+++ HibernateCrudOperation/src/main/java/com/learn/entities/User.java
@@ -2,6 +2,8 @@ package com.learn.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -11,6 +13,7 @@ public class User {
 	
 	@Id
 	@Column
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
 	
 	@Column
