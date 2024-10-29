942f6064f8d3d88cd035294100875fafe3f65510
Prasenjit <prasenjitbhosale1234@gmail.com>
2024-10-29 22:49:13 +0530

    Update operation completed

--- HibernateCrudOperation/src/main/java/com/learn/App.java
+++ HibernateCrudOperation/src/main/java/com/learn/App.java
@@ -27,12 +27,12 @@ public class App
 //    	 user2.setGender("female");
 //    	 user2.setCity("Westview");
 
-    	User user3 = new User();
-    	user3.setName("Peter Parker"); 
-    	user3.setEmail("peter.parker@gmail.com");
-    	user3.setPassword("webslinger2023");
-    	user3.setGender("male");
-    	user3.setCity("New York");
+//    	User user3 = new User();
+//    	user3.setName("Peter Parker"); 
+//    	user3.setEmail("peter.parker@gmail.com");
+//    	user3.setPassword("webslinger2023");
+//    	user3.setGender("male");
+//    	user3.setCity("New York");
 
 
         
@@ -59,30 +59,50 @@ public class App
 //    	********************
     	// --------- select operation ---------
     	
-    	try {
-    		// retrives data of user with id 1 in database . >>'1L' is added cause Long data type in User entity
-    		User user = session.get(User.class, 1L);
-    		
-    		if(user != null) {
-    			System.out.println(user.getId());
-        		System.out.println(user.getName());
-        		System.out.println(user.getEmail());
-        		System.out.println(user.getPassword());
-        		System.out.println(user.getGender());
-        		System.out.println(user.getCity());
-    		}
-    		else {
-    			System.out.println("User Not Found !");
-    		}
-    		
-    		
-    		
-    	}
-    	catch(Exception e) {
-    		e.printStackTrace()
-    	}
+//    	try {
+//    		// retrives data of user with id 1 in database . >>'1L' is added cause Long data type in User entity
+//    		User user = session.get(User.class, 1L);
+//    		
+//    		if(user != null) {
+//    			System.out.println(user.getId());
+//        		System.out.println(user.getName());
+//        		System.out.println(user.getEmail());
+//        		System.out.println(user.getPassword());
+//        		System.out.println(user.getGender());
+//        		System.out.println(user.getCity());
+//    		}
+//    		else {
+//    			System.out.println("User Not Found !");
+//    		}
+//    		
+//    		
+//    		
+//    	}
+//    	catch(Exception e) {
+//    		e.printStackTrace()
+//    	}
      	
 //    	***********
+    	
+    	// --------Update  Operation ------- 
+    	
+    	// updating city for id 2 
+    	User user = session.get(User.class, 2L);
+    	user.setCity("Lahore");
+    	
+    	
+    	
+    	try {
+    		session.saveOrUpdate(user);
+        	transaction.commit();
+        	
+        	System.out.println("USER DETAILS Updated SUCCESSFULLY !");
+    	}
+    	catch(Exception e) {
+    		transaction.rollback();
+    		e.printStackTrace();
+    		System.out.println("USER DETAILS NOT Updaed DUE TO SOME ERROR !");
+    	}
     	
     	
     	
