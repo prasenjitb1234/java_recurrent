1a059c5d7eb76f8a2c8f04227a787fb5af367d65
Prasenjit <prasenjitbhosale1234@gmail.com>
2024-10-30 12:10:27 +0530

    added Select Operation

--- MyHibernateProject/src/main/java/com/test/App.java
+++ MyHibernateProject/src/main/java/com/test/App.java
@@ -12,13 +12,13 @@ import com.test.entities.User;
 public class App {
 	public static void main(String[] args) {
 		
-		User user1 = new User();
-		user1.setId(1);
-		user1.setName("prasenjit");
-		user1.setEmail("prasenjit@gmail.com");
-		user1.setPassword("12345");
-		user1.setGender("male");
-		user1.setCity("mumbai");
+//		User user1 = new User();
+//		user1.setId(1);
+//		user1.setName("prasenjit");
+//		user1.setEmail("prasenjit@gmail.com");
+//		user1.setPassword("12345");
+//		user1.setGender("male");
+//		user1.setCity("mumbai");
 		
 		
 //_____ ____ ____ ________ ______ ______ ____ _________ ______________ ______
@@ -31,19 +31,47 @@ public class App {
 		 Session session = sessionFactory.openSession();
 		 Transaction transaction = session.beginTransaction();
 		
+		 // Insert 
+//		 try {
+//			 session.save(user1);
+//			 transaction.commit();
+//			 System.out.println("User Details saved Successfully");
+//			 
+//		 }
+//		 catch(Exception e) {
+//			 transaction.rollback();
+//			 e.printStackTrace();
+//			 System.out.println("USER DETAILS NOT ADDED DUE TO SOME ERROR");
+//		 }
 		
+//		 **************
+		 
+		 // Select 
+		 
 		 try {
-			 session.save(user1);
-			 transaction.commit();
-			 System.out.println("User Details saved Successfully");
+			 
+		 User user = session.get(User.class, 1);
+		 
+		 if(user != null) {
+			 System.out.println(user.getId());
+			 System.out.println(user.getName());
+			 System.out.println(user.getEmail());
+			 System.out.println(user.getPassword());
+			 System.out.println(user.getGender());
+			 System.out.println(user.getCity());
 			 
 		 }
-		 catch(Exception e) {
-			 transaction.rollback();
-			 e.printStackTrace();
-			 System.out.println("USER DETAILS NOT ADDED DUE TO SOME ERROR");
+		 else {
+			 System.out.println("User not found");
 		 }
-		
+		 
+		 }
+		 catch(Exception e){
+			 e.printStackTrace();
+		 }
+		 
+		 
+		 
 		
 		
 		
