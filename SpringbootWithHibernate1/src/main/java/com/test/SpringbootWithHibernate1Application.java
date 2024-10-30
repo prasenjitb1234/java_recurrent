3deb5ec8c59894f289269bbada03cef52a0ef201
Prasenjit <prasenjitbhosale1234@gmail.com>
2024-10-30 13:10:24 +0530

    updated SpringBoot Hibernate1

--- SpringbootWithHibernate1/src/main/java/com/test/SpringbootWithHibernate1Application.java
+++ SpringbootWithHibernate1/src/main/java/com/test/SpringbootWithHibernate1Application.java
@@ -1,13 +1,37 @@
 package com.test;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.test.entities.User;
 
 @SpringBootApplication
 public class SpringbootWithHibernate1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootWithHibernate1Application.class, args);
+		
+		
+		Configuration cfg = new Configuration();
+		
+		cfg.configure("/com/test/config/hibernate.cfg.xml");
+		
+		SessionFactory sessionFactory = cfg.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+//		Transaction transaction = session.beginTransaction(); 
+		
+		User user = session.get(User.class, 1);
+		System.out.println(user.getName());
+		System.out.println(user.getEmail());
+		
+		
+		
+		
+		
 	}
 
 }
